85920d14345b36b2d16928f3301a7684c68047c7
Daniel Melo <melocarrillodaniel@gmail.com>
2025-05-09 10:54:14 -0500

    fix?(): check in github actions if solved

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-//import remarkMath from "remark-math";
-//import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://dmeloca.github.io',
 	base: 'Root-Write',
-//	markdown: {
-//		remarkPlugins: [remarkMath],
-//		rehypePlugins: [rehypeKatex]
-//	},
-	integrations: [mdx(), sitemap()]
+    markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex]
+	},
+	integrations: [sitemap(), starlight({
+		customCss: ["katex/dist/katex.min.css"],
+		title: ''
+	}),mdx()]
 });
